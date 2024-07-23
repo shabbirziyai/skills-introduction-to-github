6b05cc8964c439f1c9d34452826c0f2a08e326be
shabbirziyai <shabbir.ziyai.17@gmail.com>
2024-07-24 00:17:04 +0530

    add a 3rd print statement to test2.py

--- test2.py
+++ test2.py
@@ -1,3 +1,4 @@
 # Test python file to print a statement.
-print ("Hello World and Bye in test2.py!!")
-print ("Hello World and Bye in test2.py!!")
+print ("Hello World and Bye 1 in test2.py!!")
+print ("Hello World and Bye 2 in test2.py!!")
+print ("Hello World and Bye 3 in test2.py!!")
